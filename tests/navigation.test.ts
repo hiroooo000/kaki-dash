28c6ef00787ba47d310b41b6def1739f0d610628
hiroooo000 <hiroooo000@gmail.com>
2026-01-23 15:29:53 +0000

    chore: Remove unused eslint-disable directives

--- tests/navigation.test.ts
+++ tests/navigation.test.ts
@@ -2,7 +2,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
+
 /* eslint-disable @typescript-eslint/unbound-method */
 import { describe, it, expect, beforeEach } from 'vitest';
 import { Kakidash } from '../src/index';
--- tests/repro_display_shift.test.ts
+++ tests/repro_display_shift.test.ts
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 
 import { describe, it, expect, beforeEach, afterEach, vi } from 'vitest';
 import { Kakidash } from '../src/index';
